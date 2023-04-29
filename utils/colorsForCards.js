0abe80941cfc9d21ef67e9200b2cb150bf55bcd9
Danil <danilater0105@gmail.com>
2023-04-29 19:12:53 +0300

    first commit

---
+++ utils/colorsForCards.js
@@ -0,0 +1,23 @@
+const colorsForFrontImages = [
+  '#0000FF',
+  '#FF0000',
+  '#FFC0CB', 
+  '#FFFF00', 
+  '#800080', 
+  '#008000', 
+  '#00FFFF', 
+  '#FFFFFF', 
+  '#0000FF', 
+  '#FF0000', 
+  '#FFC0CB', 
+  '#FFFF00', 
+  '#800080', 
+  '#008000', 
+  '#00FFFF', 
+  '#FFFFFF' 
+];
+
+  
+
+
+export default colorsForFrontImages;
